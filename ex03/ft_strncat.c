055159535cb38f3421cce5cde573acb97399944c
Emily Vavrinu <evavrinu@c1r7s1.42prague.com>
2026-05-25 18:14:48 +0200

    The C library strncat() function takes three variable as parameters which appends the string pointed to by src to the end of the string pointed to by dest up to n characters long.

---
+++ ex03/ft_strncat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evavrinu <evavrinu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/25 18:08:07 by evavrinu          #+#    #+#             */
+/*   Updated: 2026/05/25 18:14:12 by evavrinu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (j < nb && src[j] != '\0')
+	{
+		dest[i] = dest[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+// int	main(void)
+// {
+// 	char	dest[10] = "dest";
+// 	char	src[10] = "src";
+// 	int		n;
+
+// 	n = 2;
+// 	ft_strncat(dest, src, n);
+// 	printf("%s", dest);
+// }
