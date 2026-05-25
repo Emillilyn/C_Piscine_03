b54636d41c193ab6024864e76c66d53a0b3dae8b
Emily Vavrinu <evavrinu@c1r7s1.42prague.com>
2026-05-25 18:26:45 +0200

    The strstr() function returns a pointer to the position of the first occurrence of a string in another string.

---
+++ ex04/ft_strstr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evavrinu <evavrinu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/25 18:16:08 by evavrinu          #+#    #+#             */
+/*   Updated: 2026/05/25 18:23:47 by evavrinu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (to_find[0] == '\0')
+		return (str);
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] != '\0' && str[i + j] == to_find[j])
+		{
+			if (to_find[j + 1] == '\0')
+				return (&str[i]);
+			++j;
+		}
+		++i;
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	char *str = "Try to find this part of the text";
+	char *to_find = "to find";
+
+	printf("Returned: %s\n", ft_strstr(str, to_find));
+}
