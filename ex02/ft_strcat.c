5d4bf4f02816e402814985decd6776e62a1f979b
Emily Vavrinu <evavrinu@c1r7s1.42prague.com>
2026-05-25 17:28:35 +0200

    The strcat() function appends a copy of one string to the end of another. The strcat() function is defined in the <string. h> header file.

---
+++ ex02/ft_strcat.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evavrinu <evavrinu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/25 17:06:51 by evavrinu          #+#    #+#             */
+/*   Updated: 2026/05/25 17:27:38 by evavrinu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+// int	main(void)
+// {
+// 	char	dest[10] = "dest";
+// 	char	src[10] = "src";
+
+// 	ft_strcat(dest, src);
+// 	printf("String: %s\n", dest);
+// }
